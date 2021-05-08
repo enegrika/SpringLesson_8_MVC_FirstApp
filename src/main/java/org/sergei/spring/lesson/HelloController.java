8ba060276c9d3dcda37c478ea266c51ba71c0578
enegrika <sergeigroshev@gmail.com>
2021-05-08 17:04:17 +0700

    add <mvc:annotation-driven> thymeleaf.spring5

--- src/main/java/org/sergei/spring/lesson/HelloController.java
+++ src/main/java/org/sergei/spring/lesson/HelloController.java
@@ -1,4 +1,13 @@
 package org.sergei.spring.lesson;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/helloMVC")
+    public String sayHello(){
+        return "hello_world";
+    }
 }
