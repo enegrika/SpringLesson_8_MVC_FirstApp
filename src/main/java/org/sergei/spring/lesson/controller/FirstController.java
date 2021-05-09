ce7d645a93c2163c415f297a3eed74a637768c9f
enegrika <sergeigroshev@gmail.com>
2021-05-09 18:09:14 +0700

    add @RequestMapping to /first

--- src/main/java/org/sergei/spring/lesson/controller/FirstController.java
+++ src/main/java/org/sergei/spring/lesson/controller/FirstController.java
@@ -2,9 +2,12 @@ package org.sergei.spring.lesson.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")// ЗДЕСЬ СМОЖЕМ ОБРАТИТЬСЯ В ПАПКУ first
 public class FirstController {
+
     @GetMapping("/hell1")    // URL для ЗАПРОСА в БРАУЗЕРЕ http://localhost:8080/hell1
     public String helloPage() {
         return "first/hello"; // ПУТЬ К ФАЙЛУ в ПРЕДСТАВЛЕНИИ (папка first файл hello.html)
