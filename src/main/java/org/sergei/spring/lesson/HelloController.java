138fbb954a374d6a61f95e88200c7ff7422c006c
enegrika <sergeigroshev@gmail.com>
2021-05-09 17:35:22 +0700

    add RequestMapping annotation

--- src/main/java/org/sergei/spring/lesson/HelloController.java
+++ src/main/java/org/sergei/spring/lesson/HelloController.java
@@ -2,11 +2,16 @@ package org.sergei.spring.lesson;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
+    /// возвращает имя HTML файла в нашем WEB-INF/views
     @GetMapping("/hell")
+
+//    @RequestMapping(method = RequestMethod.GET) - УСТАРЕВШИЙ СПОСОБ иногда может использоваться на классе
     public String sayHello(){
         return "hello_world";
     }
