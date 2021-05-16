717ecb935600a1523cf4475960c1a497c67b31af
enegrika <sergeigroshev@gmail.com>
2021-05-16 10:46:32 +0700

    add MyControleer for ThymeLeaf tests

---
+++ src/main/java/org/sergei/spring/lesson/controller/MyController.java
@@ -0,0 +1,31 @@
+package org.sergei.spring.lesson.controller;
+
+import org.sergei.spring.lesson.DAO.PersonDAO;
+import org.sergei.spring.lesson.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public MyController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping("/my")
+    public String anyMethod(Model model) {
+
+        Person p1 = new Person(23, "Bobby");
+        model.addAttribute("obj", 2363246);
+        model.addAttribute("p1", p1);
+        model.addAttribute("personList",personDAO.personList());
+        model.addAttribute("string","anyString.....");
+        return "myhtml";
+    }
+
+}
