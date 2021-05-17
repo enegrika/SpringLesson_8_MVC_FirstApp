e9fd4908db3eaad48830ab555e69e8314f2c1426
Sergey Groshev <sergeigroshev@gmail.com>
2021-05-17 10:05:32 +0700

    update

--- src/main/java/org/sergei/spring/lesson/controller/MyController.java
+++ src/main/java/org/sergei/spring/lesson/controller/MyController.java
@@ -10,13 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController {
 
-    @GetMapping("/my")
-    public String inde(Model model) {
-
-//        Person p = new Person(25, "Robert");
-//        model.addAttribute("p", p);
-        return "myHtmlFile";
-    }
     private final PersonDAO personDAO;
 
     @Autowired
