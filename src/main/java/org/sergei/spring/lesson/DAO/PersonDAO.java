ddce59ab10d2e5ddf6ea60f8a615784f2838e1f6
Sergey Groshev <sergeigroshev@gmail.com>
2021-05-18 18:16:08 +0700

    add PREPARED STATEMENT to Dynamic SQL queries

--- src/main/java/org/sergei/spring/lesson/DAO/PersonDAO.java
+++ src/main/java/org/sergei/spring/lesson/DAO/PersonDAO.java
@@ -67,11 +67,27 @@ public class PersonDAO {
 
 
     public Person getPersonById(int id) {
-//        return personList.stream()
-//                .filter(person -> person.getId() == id)
-//                .findAny()
-//                .orElse(null);
-        return null;
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+
+            // POLUCHAEM posle vypolneniya SQL zaprosa - RESULTSET object
+            // berem iz nego znacheniya kolonok  i prisvaivaem vremennomu obiektu person
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+
+            person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return person;
     }
 
     public void save(Person person) {
@@ -79,13 +95,23 @@ public class PersonDAO {
 //        personList.add(person);
 
         try {
-            Statement statement = connection.createStatement();
-            String SQL = "INSERT INTO Person VALUES(" + 1
-                    + ",'" + person.getName()
-                    + "'," + person.getAge()
-                    + ",'" + person.getEmail()
-                    + "')";
-            statement.executeUpdate(SQL);
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Person VALUES ( 1, ?, ?, ?);");
+
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+
+            preparedStatement.executeUpdate();
+
+
+//            Statement statement = connection.createStatement();
+//            String SQL = "INSERT INTO Person VALUES(" + 1
+//                    + ",'" + person.getName()
+//                    + "'," + person.getAge()
+//                    + ",'" + person.getEmail()
+//                    + "')";
+//            statement.executeUpdate(SQL);//
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
