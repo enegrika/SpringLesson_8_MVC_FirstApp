46b4590ce3d8ca6181ba5ca421d3892d4026ab78
Sergey Groshev <sergeigroshev@gmail.com>
2021-05-19 10:30:30 +0700

    add EDIT and DELETE prepared staements

--- src/main/java/org/sergei/spring/lesson/DAO/PersonDAO.java
+++ src/main/java/org/sergei/spring/lesson/DAO/PersonDAO.java
@@ -9,7 +9,16 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PERSON_COUNT;
+//    private static int PERSON_COUNT;
+//    private List<Person> personList;
+//      {
+//        personList = new ArrayList<>();
+//        personList.add(new Person(++PERSON_COUNT, "Сергей",25,"ser@dsfsd.com"));
+//        personList.add(new Person(++PERSON_COUNT, "Sergei",56,"gri@dsfsd.com"));
+//        personList.add(new Person(++PERSON_COUNT, "Ivan",76,"ivan@dsfsd.com"));
+//        personList.add(new Person(++PERSON_COUNT, "Bobby",23,"bob@dsfsd.com"));
+//        personList.add(new Person(++PERSON_COUNT, "Denis",43,"denis@dsfsd.com"));
+//    }
 
     private static final String URL = "jdbc:postgresql://localhost:5432/SpringMVC_DB";
     private static final String USERNAME = "postgres";
@@ -32,18 +41,10 @@ public class PersonDAO {
     }
 
 
-//    private List<Person> personList;
-//      {
-//        personList = new ArrayList<>();
-//        personList.add(new Person(++PERSON_COUNT, "Сергей",25,"ser@dsfsd.com"));
-//        personList.add(new Person(++PERSON_COUNT, "Sergei",56,"gri@dsfsd.com"));
-//        personList.add(new Person(++PERSON_COUNT, "Ivan",76,"ivan@dsfsd.com"));
-//        personList.add(new Person(++PERSON_COUNT, "Bobby",23,"bob@dsfsd.com"));
-//        personList.add(new Person(++PERSON_COUNT, "Denis",43,"denis@dsfsd.com"));
-//    }
-
     public List<Person> getAllpersonsList() {
         List<Person> personList = new ArrayList<>();
+
+        // HERE WE CAN USE normal STATEMENT object because SQL query here is STATIC - can not change dynamically
         try {
             Statement statement = connection.createStatement();
             String SQL = "SELECT * FROM Person";
@@ -119,14 +120,41 @@ public class PersonDAO {
     }
 
     public void edit(int id, Person updatedPerson) {
-        Person personForUpdate = getPersonById(id);
+//        Person personForUpdate = getPersonById(id);
+//
+//        personForUpdate.setAge(updatedPerson.getAge());
+//        personForUpdate.setName(updatedPerson.getName());
+//        personForUpdate.setEmail(updatedPerson.getEmail());
 
-        personForUpdate.setAge(updatedPerson.getAge());
-        personForUpdate.setName(updatedPerson.getName());
-        personForUpdate.setEmail(updatedPerson.getEmail());
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
+
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public void delete(int id) {
 //        personList.removeIf(person -> person.getId() == id);
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("DELETE FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
